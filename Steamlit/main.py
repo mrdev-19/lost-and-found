b24c57d22d99ab2f2819aea436f5258e667ce463
Chanakya Devendra <101084514+mrdev-19@users.noreply.github.com>
2023-01-09 22:58:16 +0530

    Update main.py

--- Steamlit/main.py
+++ Steamlit/main.py
@@ -27,8 +27,8 @@ curlogin=""
 def log_sign():
     selected=option_menu(
         menu_title=None,
-        options=["Login","signup"],
-        icons=["bi bi-link-45deg","bi bi-arrow-bar-up "],
+        options=["Login","Signup","Admin"],
+        icons=["bi bi-link-45deg","bi bi-arrow-bar-up ","bi-person-badge-fill"],
         orientation="horizontal"
     )
     global submit
@@ -49,10 +49,11 @@ def log_sign():
                     else:
                         st.error("Please check your username / password ")
             
-    elif(selected=="signup"):
-        with st.form("Sign Up",clear_on_submit=True):
+    elif(selected=="Signup"):
+         with st.form("Sign Up",clear_on_submit=True):
             st.header("Sign Up")
             email=st.text_input("Enter your email")
+            number=st.text_input("Enter your Mobile Number")
             username=st.text_input("Enter your username")
             password=st.text_input("Enter your password",type="password")
             submit=st.form_submit_button()
@@ -60,20 +61,80 @@ def log_sign():
                 dev=db.fetch_all_users()
                 usernames=[]
                 emails=[]
+                numbers=[]
                 for user in dev:
                     usernames.append(user["key"])
                     emails.append(user["email"])
+                    numbers.append(user["number"])
                 if(username in usernames):
                     st.error("Username already exists!\nTry another username !")
                 elif(email in emails):
                     st.error("email already exists!\nTry with another email !")
+                elif(number in numbers):
+                    st.error("Phone number already exists\nTry with another number")
                 elif(len(password)<=6):
                     st.error("Password cannot be less than 6 characters")
                 else:
-                    db.insert_user(username,password,email)
+                    db.insert_user(username,password,email,number)
                     st.success("Signed Up Successfully")
 
+    elif selected=="Admin":
+        with st.form("Admin Login",clear_on_submit=True):
+            st.header("Admin Login")
+            username=st.text_input("Username")
+            password=st.text_input("Password",type="password")
+            submit=st.form_submit_button()
+            if(submit):
+                if(username=="" or password==""):
+                    st.warning("Enter your login credentials")
+                else:
+                    if(db.ad_authenticate(username,password)):
+                        st.session_state["curlogin"]=username
+                        st.session_state["key"]="adminmain"
+                        st.experimental_rerun()
+                    else:
+                        st.error("Please check your username / password ")
 def main():
+    selected=option_menu(
+            menu_title=None,
+            options=["Lost","Found"],
+            icons=["bi bi-search","bi bi-box"],
+            orientation="horizontal"
+        )
+    if(selected=="Lost"):
+        
+        with st.form("Lost entry form_@dev_mr",clear_on_submit=True):
+            st.header("Lost Item Form")
+            date=st.date_input("Enter the date on which the object is Lost")
+            name=st.text_input("Enter the name of the object Lost",placeholder="Phone etc.")
+            place=st.text_input("Where was it lost ?",placeholder="Atrium / XLab etc.")
+            mailid=st.text_input("Email !",placeholder="yourname@org.com")
+            other=st.text_input("Any other relevant details? ",placeholder="Colour / Size / Specifications etc.")
+            submitted=st.form_submit_button("Submit data")
+            lof="lost"
+            if(submitted):
+                if(name=="" or place==""):
+                    st.error("Enter All Required Fields")
+                else:
+                    db.insert_entry(st.session_state["curlogin"],str(date),name,place,mailid,other,lof)
+                    st.success("Form Submitted Successfully")
+
+    elif(selected=="Found"):
+        with st.form("found_entry_form@devmr",clear_on_submit=True):
+            st.header("Found Item Form")
+            date=st.date_input("Enter the date on which the object is found")
+            name=st.text_input("Enter the name of the object found",placeholder="Phone etc.")
+            place=st.text_input("Where is it found ?",placeholder="Atrium / XLab etc.")
+            mailid=st.text_input("Email !",placeholder="yourname@org.com")
+            other=st.text_input("Any other relevant details? ",placeholder="Colour / Size / Specifications etc.")
+            lof="found"
+            submitted=st.form_submit_button("Submit data")
+            if(submitted):
+                #write the data inputted to database:
+                db.insert_entry(st.session_state["curlogin"],str(date),name,place,mailid,other,lof)
+                st.success("Form Submitted Successfully")
+
+def admin():
     selected=option_menu(
             menu_title=None,
             options=["Lost","Found"],
@@ -88,7 +149,7 @@ def main():
             if(user["status"]==""):
                 items.append(user["name"])
         option=st.selectbox(
-            "Claim a Lost Item : ",
+            "Found Items List : ",
             tuple(items),
             0
         )
@@ -96,16 +157,16 @@ def main():
         for user in lost:
             if(user["name"]==option):
                 with st.form("ClaimLostObject@mrdev19",clear_on_submit=True):
-                    st.subheader("Claim Lost Item")
+                    st.subheader("Found Items List")
                     st.write("Item : "+user["name"])
                     st.write("Place Where it is Lost : "+user["place"])
                     st.write("Other Details : "+user["other"])
-                    check=st.checkbox("I Accept that this claim is legit")
-                    sub=st.form_submit_button("Claim")
-                    if(sub and check):
-                        st.write("Your Item was found by "+user["username"]+" You can contact him\her at "+user["number"])
-                        st.warning("The Contact will be shown only once for security reasons , kindly note the contact details")
-                        return db.f_change_status(user["key"],st.session_state["curlogin"])
+                    
+                    rollnumb=st.text_input("Enter the roll of the student to whom it was returned")
+                    check=st.checkbox("The item is returned to the owner")        
+                    sub=st.form_submit_button("Returned Successfully")
+                    if(sub and check and rollnumb):
+                        return db.f_change_status(user["key"],rollnumb)
         st.write("")
         st.write("")
         st.write("")
@@ -123,7 +184,7 @@ def main():
             date=st.date_input("Enter the date on which the object is Lost")
             name=st.text_input("Enter the name of the object Lost",placeholder="Phone etc.")
             place=st.text_input("Where was it lost ?",placeholder="Atrium / XLab etc.")
-            number=st.text_input("Enter Your Mobile Number ",placeholder="+91 XXXXXXXXXX")
+            mailid=st.text_input("Email !",placeholder="yourname@org.com")
             other=st.text_input("Any other relevant details? ",placeholder="Colour / Size / Specifications etc.")
             submitted=st.form_submit_button("Submit data")
             lof="lost"
@@ -131,7 +192,7 @@ def main():
                 if(name=="" or place==""):
                     st.error("Enter All Required Fields")
                 else:
-                    db.insert_entry(st.session_state["curlogin"],str(date),name,place,number,other,lof)
+                    db.insert_entry(st.session_state["curlogin"],str(date),name,place,mailid,other,lof)
                     st.success("Form Submitted Successfully")
 
     elif(selected=="Found"):
@@ -142,7 +203,7 @@ def main():
             if(user["status"]==""):
                 items.append(user["name"])
         option=st.selectbox(
-            "Return a Found Item : ",
+            "Lost items list",
             tuple(items),
             0
         )
@@ -154,11 +215,14 @@ def main():
                     st.write("Item : "+user["name"])
                     st.write("Place Where it is found : "+user["place"])
                     st.write("Other Details : "+user["other"])
-                    sub=st.form_submit_button("Return")
-                    if(sub):
-                        st.write("The Item belongs to "+user["username"]+" You can contact him\her at "+user["number"])
-                        st.warning("The Contact will be shown only once for security reasons , kindly note the contact details")
-                        return db.l_change_status(user["key"],st.session_state["curlogin"])
+                    st.write("The Item belongs to "+user["username"])
+                    st.markdown('<a href="mailto:'+user["email"]+'">Contact</a>', unsafe_allow_html=True)
+                    # st.write("You can contact him\her at "+user["number"])
+                    rollnum=st.text_input("Enter the roll number of the student whom it was returned by ",placeholder="")
+                    sub=st.form_submit_button("Return The Item to the Owner")
+                    if(sub and rollnum):
+                        return db.l_change_status(user["key"],rollnum)
+
         st.write("")
         st.write("")
         st.write("")
@@ -176,19 +240,24 @@ def main():
             date=st.date_input("Enter the date on which the object is found")
             name=st.text_input("Enter the name of the object found",placeholder="Phone etc.")
             place=st.text_input("Where is it found ?",placeholder="Atrium / XLab etc.")
-            number=st.text_input("Enter Your Mobile Number ",placeholder="+91 XXXXXXXXXX")
+            mailid=st.text_input("Email !",placeholder="yourname@org.com")
             other=st.text_input("Any other relevant details? ",placeholder="Colour / Size / Specifications etc.")
             lof="found"
             submitted=st.form_submit_button("Submit data")
             if(submitted):
                 #write the data inputted to database:
-                db.insert_entry(st.session_state["curlogin"],str(date),name,place,number,other,lof)
+                db.insert_entry(st.session_state["curlogin"],str(date),name,place,mailid,other,lof)
                 st.success("Form Submitted Successfully")
+    
 
 if "key" not in st.session_state:
     st.session_state["key"] = "log_sign"
 
 if st.session_state["key"] == "log_sign":
     log_sign()
+
+elif st.session_state["key"] == "adminmain":
+    admin()
+
 elif st.session_state["key"] == "main":
     main()
