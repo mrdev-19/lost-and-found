1b48e964c12ce967591ec55c860ef1f3876777cd
Chanakya Devendra <101084514+mrdev-19@users.noreply.github.com>
2023-01-09 22:59:01 +0530

    Update database.py

--- Steamlit/database.py
+++ Steamlit/database.py
@@ -9,29 +9,33 @@ load_dotenv(".env")
 DETA_KEY=os.getenv("DETA_KEY")
 deta=Deta(DETA_KEY)
 
+
 ldb=deta.Base("Lost")
 rdb=deta.Base("Found")
 cred=deta.Base("Creds")
+admin=deta.Base("Admin")
 
-def insert_user(username,password,email):
-    username=username.strip()
-    dev=fetch_all_users()
-    usernames=[]
-    emails=[]
-    for user in dev:
-        usernames.append(user["key"])
-        emails.append(user["email"])
-    if(username in usernames):
-        return "Username already exists!\nTry another username !"
-    elif(email in emails):
-        return "email already exists!\nTry with another email !"
-    else:
-        cred.put({"key":username,"password":password,"email":email})
+def insert_user(username,password,email,number):
+    cred.put({"key":username,"password":password,"email":email,"number":number})
+
+def insert_admin(username,password,email,number):
+    admin.put({"key":username,"password":password,"email":email,"number":number})
 
 def authenticate(username,password):
-    username=username.strip()
     var=1
     dev=fetch_all_users()
+    usernames=[user["key"] for user in dev]
+    emails=[user["email"] for user in dev]
+    for user in dev:
+        if(username==user["key"] and user["password"]==password):
+            return True
+            var=0
+    if(var):
+        return False
+
+def ad_authenticate(username,password):
+    var=1
+    dev=fetch_all_admins()
     usernames=[user["key"] for user in dev]
     emails=[user["email"] for user in dev]
     for user in dev:
@@ -50,6 +54,10 @@ def fetch_all_users():
     res=cred.fetch()
     return res.items
 
+def fetch_all_admins():
+    res=admin.fetch()
+    return res.items
+
 def fetch_all_entries(username):
     data=[]
     dev=entries.fetch()
@@ -59,11 +67,11 @@ def fetch_all_entries(username):
             data.append({"Entry":user["data"],"Date":user["date"]})
     return data
 
-def insert_entry(username,date,name,place,number,other,lof):
+def insert_entry(username,date,name,place,mailid,other,lof):
     if(lof=="lost"):
-        return ldb.put({"date": date,"name": name,"place": place,"number":number,"username":username,"other": other,"status":""})
+        return ldb.put({"date": date,"name": name,"place": place,"email":mailid,"username":username,"other": other,"status":""})
     else:
-        return rdb.put({"date": date,"name": name,"place": place,"number":number,"username":username,"other": other,"status":""})
+        return rdb.put({"date": date,"name": name,"place": place,"email":mailid,"username":username,"other": other,"status":""})
 
 def all_lost():
     dev=ldb.fetch()
